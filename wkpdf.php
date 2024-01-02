645a8476bc679ca52a8128751b3346f24acf6495
yehudaclinton <yehudaclinton@gmail.com>
2024-01-02 20:44:03 +0200

    Update wkpdf.php
    most features now conceptually function

--- wkpdf.php
+++ wkpdf.php
@@ -29,34 +29,25 @@ $pdf = new Pdf(array(
     'binary' => '/usr/bin/wkhtmltopdf',
     //'no-outline',         // Make Chrome not complain
 
-    //'margin-top'    => 15, //doing it in css page
-    //'margin-right'  => 15,
-    //'margin-bottom' => 15,
-    //'margin-left'   => 15,
-
-    // Default page options
     //'disable-smart-shrinking',
 
     'ignoreWarnings' => true,
     'commandOptions' => array(
-        'useExec' => true,      // Can help on Windows systems
         'procEnv' => array(
-            // Check the output of 'locale -a' on your system to find supported languages
             'LANG' => 'en_US.utf-8',
         ),
     ),
-
     'user-style-sheet' => 'wk.css',
 ));
 
 $sefer="<div style='text-align: center; padding-bottom: 15px'>שולחן ערוך יורה דעה</div>";
-$pg=array(1); // pageNum, siman, sif
+$pg=array(1,1); // pageNum, siman, sif
 
 $length=1;//until we actually start getting the data and find out how long it is
 for($pp=1; $pp<11; $pp++){//=$length
 $plen=0;
   for($sn=1; $sn<=$length; $sn++){
-    $url = "https://www.sefaria.org/api/texts/Shulchan_Arukh%2C_Yoreh_De'ah.$sn";
+    $url = "https://www.sefaria.org/api/texts/Shulchan_Arukh%2C_Yoreh_De'ah.$pg[1]";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -73,34 +64,35 @@ $plen=0;
       //$plen = $plen+mb_strlen($sif, 'UTF-8');
       //if($plen>3000) break;
     }
-  //$pp=$sn+1;
-  $pg[1]=$sn;
-  $plen = $plen+mb_strlen($sif, 'UTF-8');
-  if($plen>20000) break;
+    //$pp=$sn+1;
+    $pg[1]++;
+    $plen = $plen+mb_strlen($sif, 'UTF-8');
+    if($plen>20000) break;
 
-  //sections
-    //$sif = str_replace("<b>","<div style='font-weight: bold;'>",$sif);
-    //$sif = str_replace("</b>","</div>",$sif);
-    //$sif = strip_tags($sif,"<b><small><div><span>");
-    $sefer=$sefer."<div style='text-align: justify; padding-bottom: 15px;'><span style='font-size: 25px;'><b>".numToHebrew($sn)."</b></span>$sif</div>";//	
-  }
+    //sections
+      //$sif = str_replace("<b>","<div style='font-weight: bold;'>",$sif);
+      //$sif = str_replace("</b>","</div>",$sif);
+      //$sif = strip_tags($sif,"<b><small><div><span>");
+      $sefer=$sefer."<div style='text-align: justify; padding-bottom: 15px;'><span style='font-size: 25px;'><b>".numToHebrew($pg[1])."</b></span>$sif</div>";//	
+    }
 
-  //second half of page
-  $p="";
-  for($sn=1; $sn<=$pg[1]; $sn++){
-  $url2 = "https://www.sefaria.org/api/texts/Ba'er_Hetev_on_Shulchan_Arukh,_Yoreh_De'ah.".$sn;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url2);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $result = curl_exec($ch);
-    if (!$result) exit('cURL Error: ' . curl_error($ch));
-    curl_close($ch);
-    $data = json_decode($result, true);
-    $p=$p.strip_tags($data['he'][$sn],"<b><div>");
-  }
-    $secondp = "<div style='text-align: center; padding-bottom: 15px;'>פירוש</div>".$p;
+    //second half of page
+    $p="";
+    //for($sn=1; $sn<=$pg[1]; $sn++){
+      $url2 = "https://www.sefaria.org/api/texts/Ba'er_Hetev_on_Shulchan_Arukh,_Yoreh_De'ah.".$pg[1];
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url2);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      $result = curl_exec($ch);
+      if (!$result) exit('cURL Error: ' . curl_error($ch));
+      curl_close($ch);
+      $data = json_decode($result, true);
+      $p=$p.strip_tags($data['he'][$sn],"<b><div>");
+    //}
+      $secondp = "<div style='text-align: center; padding-bottom: 15px;'>פירוש</div>".$p;
+  /**/
 
-  $pages = $pages."<div>".$sefer."</div><div>".$secondp."</div>page: ".$pg[0];
+  $pages = $pages."<div>".$sefer."</div><div>".$secondp."</div>page: ".$pp;
   
 }
 
