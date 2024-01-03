56c91dfc221f3cdc348c0baa616f6135b3b88152
yehudaclinton <yehudaclinton@gmail.com>
2024-01-03 12:13:50 +0200

    Update wkpdf.php
    fixed some errs

--- wkpdf.php
+++ wkpdf.php
@@ -41,16 +41,22 @@ $pdf = new Pdf(array(
 ));
 
 $sefer="<div style='text-align: center; padding-bottom: 15px'>שולחן ערוך יורה דעה</div>";
-$pg=array(1,1); // pageNum, siman, sif
+$pg=array(1,1); // pageNum, sn, sif
 
-$length=1;//until we actually start getting the data and find out how long it is
+$length=1;//until we actually get the data and find out how long it is
 for($pp=1; $pp<11; $pp++){//=$length
+$first = true;
 $plen=0;
-  for($sn=1; $sn<=$length; $sn++){
-    $url = "https://www.sefaria.org/api/texts/Shulchan_Arukh%2C_Yoreh_De'ah.$pg[1]";
+  for($sn=$pg[1]; $sn<=$length; $sn++){ //$pg[1]
+
+    $url1 = "https://www.sefaria.org/api/texts/Shulchan_Arukh%2C_Yoreh_De'ah.$sn";
+    if($first){
+      $sn++;
+      $first = false;
+    } 
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $url1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $result = curl_exec($ch);
     if (!$result) exit('cURL Error: ' . curl_error($ch));
@@ -65,21 +71,21 @@ $plen=0;
       //if($plen>3000) break;
     }
     //$pp=$sn+1;
-    $pg[1]++;
     $plen = $plen+mb_strlen($sif, 'UTF-8');
+    $pg[1] = $sn+1;
     if($plen>20000) break;
 
     //sections
-      //$sif = str_replace("<b>","<div style='font-weight: bold;'>",$sif);
-      //$sif = str_replace("</b>","</div>",$sif);
-      //$sif = strip_tags($sif,"<b><small><div><span>");
-      $sefer=$sefer."<div style='text-align: justify; padding-bottom: 15px;'><span style='font-size: 25px;'><b>".numToHebrew($pg[1])."</b></span>$sif</div>";//	
-    }
+    //$sif = str_replace("<b>","<div style='font-weight: bold;'>",$sif);
+    //$sif = str_replace("</b>","</div>",$sif);
+    //$sif = strip_tags($sif,"<b><small><div><span>");
+    $sefer=$sefer."<div style='text-align: justify; padding-bottom: 15px;'><span style='font-size: 25px;'><b>".numToHebrew($sn)."</b></span>$sif</div>";	
+  }
 
     //second half of page
     $p="";
-    //for($sn=1; $sn<=$pg[1]; $sn++){
-      $url2 = "https://www.sefaria.org/api/texts/Ba'er_Hetev_on_Shulchan_Arukh,_Yoreh_De'ah.".$pg[1];
+    for($sn=1; $sn<=$pg[1]; $sn++){
+      $url2 = "https://www.sefaria.org/api/texts/Ba'er_Hetev_on_Shulchan_Arukh,_Yoreh_De'ah.$sn";
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url2);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -87,13 +93,20 @@ $plen=0;
       if (!$result) exit('cURL Error: ' . curl_error($ch));
       curl_close($ch);
       $data = json_decode($result, true);
-      $p=$p.strip_tags($data['he'][$sn],"<b><div>");
-    //}
+      //$p=$p.strip_tags($data['he'][$pg[1]],"<b><div>");
+
+
+      for($x=0; $x < sizeof($data['he']); $x++){
+        $p = $p."(".numToHebrew($x+1).")".strip_tags($data['he'][$x],"<b><div>");
+      }
+
+
+    }
       $secondp = "<div style='text-align: center; padding-bottom: 15px;'>פירוש</div>".$p;
   /**/
 
   $pages = $pages."<div>".$sefer."</div><div>".$secondp."</div>page: ".$pp;
-  
+  $pg[1]++;
 }
 
 $pdf->addPage("<html lang='he' dir='rtl'><head><meta charset='UTF-8' />".$pages."</head></html>");
